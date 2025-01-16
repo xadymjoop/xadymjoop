3398cc03ea975563a4ff3f7ee7130a38f44e11b3
xadymjoop <97101343+xadymjoop@users.noreply.github.com>
2025-01-16 19:51:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 from datetime import datetime
-
+import os
 # Titre de l'application
 st.title("Entraînement et prédiction avec RandomForest")
 
